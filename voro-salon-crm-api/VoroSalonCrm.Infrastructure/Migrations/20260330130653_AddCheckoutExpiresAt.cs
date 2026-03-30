e95298c1f722492f11e4f1063fabf6225d16c976
JordanCampos20 <jordancamposprogramador@gmail.com>
2026-03-30 10:07:52 -0300

    Criado campo de CheckoutExpiresAt

---
+++ voro-salon-crm-api/VoroSalonCrm.Infrastructure/Migrations/20260330130653_AddCheckoutExpiresAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoroSalonCrm.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCheckoutExpiresAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CheckoutExpiresAt",
+                table: "TenantSubscriptions",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckoutExpiresAt",
+                table: "TenantSubscriptions");
+        }
+    }
+}
